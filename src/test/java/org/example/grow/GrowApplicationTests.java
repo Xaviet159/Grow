9d518df238cb4eb9459bcc2a72e8b1eeab70ea52
Xavier Simonet <simonetxavier24@gmail.com>
2025-06-06 14:21:28 +0200

    First commit for Grow Enterprise

---
+++ src/test/java/org/example/grow/GrowApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.grow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
